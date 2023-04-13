844b03fc796e9d2bcf57b8a97467c06c6f00aa4f
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 12:52:11 -0500

    working

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
+    <div className="App"> 
       <h1>About Page!!!!!!!</h1>
       <form action="/">
           <button type="submit">Home</button>
