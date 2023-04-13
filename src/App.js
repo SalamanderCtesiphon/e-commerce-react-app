38b84fa161331674ff54324616530a8904b39928
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 11:55:08 -0500

    removed the links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import About from './About'
-import Shop from './Shop'
+
 
 function App() {
   return (
     <div className="App">
       <h1>HomePage!!!!!!</h1>
-      <About />
-      <Shop />
+      
     </div>
   );
 }
