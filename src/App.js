59ad14bd81a688dc0a766b6ce9306a79a3a0cedf
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 14:12:43 -0500

    roughly styled the pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HomePage!!!!!!</h1>
-      <form action="/about">
-         <button type="submit">About</button>
-      </form>
-      <form action="/shop">
-         <button type="submit">Shop</button>
-      </form>
+      <div className='page-view'>
+        <div className='header'>
+          <h1>HomePage!!!!!!</h1>
+          <div className='btn-box'>
+            <form action="/about">
+              <button type="submit">About</button>
+            </form>
+            <form action="/shop">
+              <button type="submit">Shop</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Shop.js
+++ src/Shop.js
@@ -3,13 +3,20 @@ import React from 'react'
 function Shop() {
   return (
     <div className="App">
-      <h1>Shop Page!!!!!!!</h1>
-      <form action="/">
-          <button type="submit">Home</button>
-      </form>
-      <form action="/About">
-          <button type="submit">About</button>
-      </form>
+      <div className='page-view'>
+        <div className='header'>
+          <h1>Shop Page!!!!!!!</h1>
+          <div className='btn-box'>
+            <form action="/">
+                <button type="submit">Home</button>
+            </form>
+            <form action="/About">
+                <button type="submit">About</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
