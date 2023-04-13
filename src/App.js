5ca9e756a88a641a86d8d1b1d59e76b022e2ab63
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 11:49:48 -0500

    linked the newly created pages to the app file to make sure everythig is working as intended

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import About from './About'
+import Shop from './Shop'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>HomePage!!!!!!</h1>
+      <About />
+      <Shop />
     </div>
   );
 }
