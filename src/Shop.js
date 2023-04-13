3102c7869203f0256b38e9b5c9de49ed5ef0457e
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 12:09:34 -0500

    linked all the pages together and tested the links

--- src/Shop.js
+++ src/Shop.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Shop() {
   return (
-    <div>Shop Page!!!!!</div>
+    <div>
+      <h1>Shop Page!!!!!!!</h1>
+      <form action="/">
+          <button type="submit">Home</button>
+      </form>
+      <form action="/About">
+          <button type="submit">About</button>
+      </form>
+    </div>
   )
 }
 
