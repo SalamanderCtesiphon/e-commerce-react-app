179393252bc2495bddd5e5276c921a1f4e30a1c9
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 14:28:44 -0500

    created an array of objects which contains the inventory of the store. then displayed the test inventory on the screen

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Shop() {
+  const [inventory] = useState([
+    {
+      id: 1,
+      quantity: 1,
+      title: 'M4800',
+      price: 100.00,
+    },
+    {
+      id: 2,
+      quantity: 1,
+      title: 'M5000',
+      price: 200.00,
+    },
+    {
+      id: 3,
+      quantity: 1,
+      title: 'M6200',
+      price: 300.00,
+    },
+  ])
+
+  function log() {
+    console.log(inventory[0].price)
+  }
+
   return (
     <div className="App">
       <div className='page-view'>
@@ -13,6 +39,21 @@ function Shop() {
             <form action="/About">
                 <button type="submit">About</button>
             </form>
+          </div>
+        </div>
+        <div>
+          <button onClick={() => log()}>click</button>
+          <div>{inventory[0].price}</div>
+          <div className='card-display'>
+            <ul>
+            {inventory.map((item) => {
+              return (
+                <li key={item.id}>
+                  <h3>{item.title}</h3>
+                </li>
+              )
+            })}
+            </ul>
           </div>
         </div>
       </div>
