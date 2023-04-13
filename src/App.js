d0caefb160d72d8ccd82aab85509a5bed209ddb8
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 12:08:25 -0500

    created links to navigate between the pages and test them out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>HomePage!!!!!!</h1>
-      
+      <form action="/about">
+         <button type="submit">About</button>
+      </form>
+      <form action="/shop">
+         <button type="submit">Shop</button>
+      </form>
     </div>
   );
 }
