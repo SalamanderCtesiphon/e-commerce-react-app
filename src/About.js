870fa7897f3ce6511563cd4fceb47d7553b7834c
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 12:09:08 -0500

    linked all the pages together and tested the links

--- src/About.js
+++ src/About.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About Page!!!!!!!</div>
+    <div>
+      <h1>About Page!!!!!!!</h1>
+      <form action="/">
+          <button type="submit">Home</button>
+      </form>
+      <form action="/shop">
+          <button type="submit">Shop</button>
+      </form>
+    </div>
   )
 }
 
