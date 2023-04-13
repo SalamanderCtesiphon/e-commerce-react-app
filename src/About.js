c4b4738fecee34034b0999ff4825715bef0bfb53
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 14:12:23 -0500

    roughly styled the page

--- src/About.js
+++ src/About.js
@@ -3,13 +3,19 @@ import React from 'react'
 function About() {
   return (
     <div className="App"> 
-      <h1>About Page!!!!!!!</h1>
-      <form action="/">
-          <button type="submit">Home</button>
-      </form>
-      <form action="/shop">
-          <button type="submit">Shop</button>
-      </form>
+      <div className='page-view'>
+        <div className='header'>
+          <h1>About Page!!!!!!!</h1>
+          <div className='btn-box'>
+            <form action="/">
+                <button type="submit">Home</button>
+            </form>
+            <form action="/shop">
+                <button type="submit">Shop</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
