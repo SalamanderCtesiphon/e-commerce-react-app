38c53d231648125750cd6ac76c73b84dd15c17c6
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-13 11:54:51 -0500

    added a route switch file

---
+++ src/RouteSwitch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import About from './About'
+import Shop from './Shop'
+
+function RouteSwitch() {
+  return (
+    <div>RouteSwitch</div>
+  )
+}
+
+export default RouteSwitch
