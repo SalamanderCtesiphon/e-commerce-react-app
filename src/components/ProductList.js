e920d1f960ec1a823ff8118c7046f41dbd954a50
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-15 18:16:45 -0500

    added a function to format numbers

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-function ProductList({ inventory, selectItem}) {
+function ProductList({ inventory, selectItem, format}) {
   return (
     <div>
        {inventory.map((item) => {
-                return (
-                  <li key={item.id} className='card'>
-                    <div className='left-box'>
-                      <h3>{item.title}</h3>
-                      <p>{item.price}</p>
-                    </div>
-                    
-                    <div className='select-btn' onClick={() => selectItem(item.id)}>Select Item</div>
-                  </li>
-                )
-              })}
+          return (
+            <li key={item.id} className='card'>
+              <div className='left-box'>
+                <h3>{item.title}</h3>
+                <p>Price: {format(item.price)}</p>
+              </div>
+              
+              <div className='select-btn' onClick={() => selectItem(item.id)}>Select Item</div>
+            </li>
+          )
+        })}
     </div>
   )
 }
