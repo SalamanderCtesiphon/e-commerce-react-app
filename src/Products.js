ecb499df048fac0231b46460588d700758c0e40b
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-15 20:28:48 -0500

    bug fixin

--- src/Products.js
+++ src/Products.js
@@ -21,7 +21,7 @@ const productsArray = [
 
 function getProductData(id) {
   let productData = productsArray.find(product => product.id === id)
-  if(productData == undefined) {
+  if(productData === undefined) {
     console.log('data does not exist for id')
     return undefined
   }
