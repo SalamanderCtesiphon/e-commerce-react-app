35c7fe2f7ccf078c2f4152d4db9d59da0d609504
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-16 14:36:03 -0500

    trying to get the hash router to work

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import About from './About'
 import Shop from './Shop'
 
 function RouteSwitch() {
   return (
-    <BrowserRouter basename='/e-commerce-react-app'>
+    <HashRouter >
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path='/shop' element={<Shop />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
