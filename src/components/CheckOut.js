b8990d33a347ea14c91645310664360c1ca7df07
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-16 12:45:46 -0500

    completed the basic logic for the application

---
+++ src/components/CheckOut.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { CartContext } from '../CartContext'
+import { getProductData } from '../Products'
+import { BsXLg } from 'react-icons/bs'
+
+
+function CheckOut(props) {
+  const cart = useContext(CartContext)
+  const id  = props.id
+  const quantity = props.quantity
+  const productData = getProductData(id)
+
+  return (
+    <div>
+      <h3>{productData.title}</h3>
+      <p>{quantity} total</p>
+      <p>$ {(quantity * productData.price).toFixed(2)}</p>
+      <BsXLg className='delete-button' onClick={() => cart.deleteFromCart(id)}/>
+      <hr></hr>
+    </div>
+    
+  )
+}
+
+export default CheckOut
