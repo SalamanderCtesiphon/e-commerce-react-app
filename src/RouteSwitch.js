f4f7e9a885e9930ab03268b0c06ec0376c02a933
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-16 14:54:02 -0500

    deploy application

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import About from './About'
 import Shop from './Shop'
 
 function RouteSwitch() {
   return (
-    <HashRouter >
+    <BrowserRouter basename="/e-commerce-react-app">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path='/shop' element={<Shop />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
