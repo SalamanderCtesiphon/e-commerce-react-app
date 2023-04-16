1b6d13c7b72c8060953e1186d062ca7ac1fd581f
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-15 20:40:25 -0500

    correct some errors in the code

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,99 +1,109 @@
-import React from 'react'
-import { createContext, useState } from 'react'
-import { productsArray, getProductData } from './Products'
+import { createContext, useState } from "react";
+import { productsArray, getProductData } from "./Products";
+
 
 export const CartContext = createContext({
-  items: [],
-  getProductQuantity: () => {},
-  addOneToCart: () => {},
-  removeOneFromCart: () => {},
-  deleteFromCart: () => {},
-  getTotalCost: () => {}
-})
+    items: [],
+    getProductQuantity: () => {},
+    addOneToCart: () => {},
+    removeOneFromCart: () => {},
+    deleteFromCart: () => {},
+    getTotalCost: () => {}
+});
 
 export function CartProvider({children}) {
-  const [cartProducts, setCartProducts] = useState([])
+    const [cartProducts, setCartProducts] = useState([]);
+    
+    // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]
 
-  function getProductQuantity(id) {
-    const quantity = cartProducts.find(product => product.id === id)?.quantity
+    function getProductQuantity(id) {
+        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+        
+        if (quantity === undefined) {
+            return 0;
+        }
 
-    if(quantity === undefined) {
-      return 0
+        return quantity;
     }
-    return quantity
-  }
 
-  function addOneToCart(id) {
-    const quantity = getProductQuantity(id)
+    function addOneToCart(id) {
+        const quantity = getProductQuantity(id);
 
-    if(quantity === 0) {
-      setCartProducts(
-        [
-          ...cartProducts,
-          {
-            id: id,
-            quantity: 1
-          }
-        ]
-      )
-    } else {
-      setCartProducts(
-        cartProducts.map(
-          product => product.id === id ? { ...product, quantity: product.quantity + 1}
-          :
-          product
+        if (quantity === 0) { // product is not in cart
+            setCartProducts(
+                [
+                    ...cartProducts,
+                    {
+                        id: id,
+                        quantity: 1
+                    }
+                ]
+            )
+        } else { // product is in cart
+            // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]    add to product id of 2
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id                                // if condition
+                    ? { ...product, quantity: product.quantity + 1 } // if statement is true
+                    : product                                        // if statement is false
+                )
+            )
+        }
+    }
+
+    function removeOneFromCart(id) {
+        const quantity = getProductQuantity(id);
+
+        if(quantity == 1) {
+            deleteFromCart(id);
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id                                // if condition
+                    ? { ...product, quantity: product.quantity - 1 } // if statement is true
+                    : product                                        // if statement is false
+                )
+            )
+        }
+    }
+
+    function deleteFromCart(id) {
+        // [] if an object meets a condition, add the object to array
+        // [product1, product2, product3]
+        // [product1, product3]
+        setCartProducts(
+            cartProducts =>
+            cartProducts.filter(currentProduct => {
+                return currentProduct.id != id;
+            })  
         )
-      )
     }
-  }
 
-  function deleteFromCart(id) {
-    setCartProducts(
-      cartProducts =>
-      cartProducts.filter(currentProduct => {
-        return currentProduct.id !== id
-      })
+    function getTotalCost() {
+        let totalCost = 0;
+        cartProducts.map((cartItem) => {
+            const productData = getProductData(cartItem.id);
+            totalCost += (productData.price * cartItem.quantity);
+        });
+        return totalCost;
+    }
+
+    const contextValue = {
+        items: cartProducts,
+        getProductQuantity,
+        addOneToCart,
+        removeOneFromCart,
+        deleteFromCart,
+        getTotalCost
+    }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
     )
-  }
-
-  function removeOneFromCart(id) {
-    const quantity = getProductQuantity(id)
-    if(quantity === 1) {
-      deleteFromCart(id)
-    } else {
-      setCartProducts(
-        cartProducts.map(
-          product => product.id === id ? { ...product, quantity: product.quantity - 1}
-          :
-          product
-        )
-      )
-    }
-  }
-
-  function getTotalCost() {
-    let totalCost = 0
-    cartProducts.map((cartItem) => {
-      const productData = getProductData(cartItem.id)
-      totalCost += (productData.price * cartItem.quantity)
-    })
-    return totalCost
-  }
-
-  const contextValue = {
-    items: [],
-    getProductQuantity,
-    addOneToCart,
-    removeOneFromCart,
-    deleteFromCart,
-    getTotalCost
-  }
-
-  return (
-    <CartContext.Provider value={contextValue}>
-      {children}
-    </CartContext.Provider>
-  )
 }
 
-export default CartProvider
+export default CartProvider;
