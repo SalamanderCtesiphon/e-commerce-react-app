db6c02df9da6c47ed5b86ec883f6222faf75d56d
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-15 19:34:04 -0500

    added helper functions to the context

--- src/CartContext.js
+++ src/CartContext.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext, useState } from 'react'
 import { productsArray } from './Products'
 
-const cartContext = createContext({
+const CartContext = createContext({
   items: [],
   getProductQuantity: () => {},
   addOneToCart: () => {},
@@ -10,3 +10,55 @@ const cartContext = createContext({
   deleteFromCart: () => {},
   getTotalCost: () => {}
 })
+
+export function CartProvider({children}) {
+  const [cartProducts, setCartProduct] = useState([])
+
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find(product => product.id === id)?.quantity
+
+    if(quantity === undefined) {
+      return 0
+    }
+    return quantity
+  }
+
+  function addOneToCart(id) {
+    const quantity = getProductQuantity(id)
+
+    if(quantity === 0) {
+      setCartProduct(
+        [
+          ...cartProducts,
+          {
+            id: id,
+            quantity: 1
+          }
+        ]
+      )
+    } else {
+      setCartProduct(
+        cartProducts.map(
+          product => product.id === id ? { ...product, quantity: product.quantity + 1}
+          :
+          product
+        )
+      )
+    }
+  }
+
+  const contextValue = {
+    items: [],
+    getProductQuantity,
+    addOneToCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost
+  }
+
+  return (
+    <CartContext.Provider value={contextValue}>
+      {children}
+    </CartContext.Provider>
+  )
+}
